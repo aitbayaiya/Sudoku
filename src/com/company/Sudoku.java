5474ec6c3b6b34db0ba21c14673fc5e25ab81bf7
aiyaaitbay <aiyaaitbay@gmai.com>
2022-02-24 11:28:14 +0600

    First Commit

---
+++ src/com/company/Sudoku.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import javax.swing.JFrame;
+
+public class Sudoku {
+
+    static JFrame frame;
+    static Panal p;
+    private static int[][] grid;
+    private static int[][] temp;
+    private static Random ran = new Random();
+    private static int level = 2;
+
+    public static void main(String[] args) {
+
+        grid = new int[9][9];
+        temp = new int[9][9];
+        frame = new JFrame();
+        frame.setResizable(false);
+        frame.setLocation(320, 40);
+        frame.setSize(650, 650);
+        frame.setTitle("</Sudoku Game/>");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        p = new Panal();
+        frame.setContentPane(p);
+        frame.setVisible(true);
+    }
+}
